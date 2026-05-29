608c2138af5339a72ff3390b8a774c7730011a02
Lucas <lucaspereiradelima2020@gmail.com>
2026-05-28 22:49:59 -0300

    Adicionando mais dias a uma data.

--- src/com/github/lucasdevrj/principal/Principal.java
+++ src/com/github/lucasdevrj/principal/Principal.java
@@ -6,5 +6,10 @@ public class Principal {
     public static void main(String[] args) {
         LocalDate dataDaCompra = LocalDate.now();
         LocalDate dataDaPrimeiraParcela = LocalDate.of(2026, 6, 10);
+        LocalDate dataDaSegundaParcela = dataDaPrimeiraParcela.plusDays(30);
+
+        System.out.println("Data da compra: " + dataDaCompra);
+        System.out.println("Data da primeira parcela: " + dataDaPrimeiraParcela);
+        System.out.println("Data da segunda parcela: " + dataDaSegundaParcela);
     }
 }
