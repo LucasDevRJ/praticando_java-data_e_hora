26ef9aec6e040f7f7395a8360f7ca10b4af4d6db
Lucas <lucaspereiradelima2020@gmail.com>
2026-05-28 23:16:21 -0300

    Usando métodos booleanos para comparar datas.

--- src/com/github/lucasdevrj/principal/Principal.java
+++ src/com/github/lucasdevrj/principal/Principal.java
@@ -5,11 +5,20 @@ import java.time.LocalDate;
 public class Principal {
     public static void main(String[] args) {
         LocalDate dataDaCompra = LocalDate.now();
-        LocalDate dataDaPrimeiraParcela = LocalDate.of(2026, 6, 10);
+        LocalDate dataDoRecebimentoDoSalario = LocalDate.of(2026, 5, 5);
+        LocalDate dataDaPrimeiraParcela = LocalDate.of(2026, 5, 28);
         LocalDate dataDaSegundaParcela = dataDaPrimeiraParcela.plusDays(30);
 
         System.out.println("Data da compra: " + dataDaCompra);
         System.out.println("Data da primeira parcela: " + dataDaPrimeiraParcela);
         System.out.println("Data da segunda parcela: " + dataDaSegundaParcela);
+
+        if (dataDaPrimeiraParcela.isEqual(dataDaCompra)) {
+            System.out.println("A parcela esta vencendo hoje.");
+        }
+
+        if (dataDoRecebimentoDoSalario.isBefore(dataDaPrimeiraParcela)) {
+            System.out.println("Recebe salário antes da primeira fatura.");
+        }
     }
 }
