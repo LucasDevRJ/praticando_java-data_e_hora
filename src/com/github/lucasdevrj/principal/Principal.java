128cfe5b1786eb939234fbb54ac90aea0a6c4c8d
Lucas <lucaspereiradelima2020@gmail.com>
2026-05-28 22:46:59 -0300

    Criando instâncias de Datas.

---
+++ src/com/github/lucasdevrj/principal/Principal.java
@@ -0,0 +1,10 @@
+package com.github.lucasdevrj.principal;
+
+import java.time.LocalDate;
+
+public class Principal {
+    public static void main(String[] args) {
+        LocalDate dataDaCompra = LocalDate.now();
+        LocalDate dataDaPrimeiraParcela = LocalDate.of(2026, 6, 10);
+    }
+}
